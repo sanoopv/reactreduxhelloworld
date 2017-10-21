d9aadda3d5577dcb52f72499b78957815406ee03
sanoopv <sanoopv@gmail.com>
2017-10-21 17:33:56 +0530

    initial

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux';
 import sampleReducer from './samplereducer';
 
 const rootReducer = combineReducers({
-    authData: authReducer,
+    
     sampleData: sampleReducer
 });
 export default rootReducer;
